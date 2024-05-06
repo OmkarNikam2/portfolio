7442fc7ce864bb0256329bed42218b5449b0891c
snehasharma <sneha.sharma@1click.tech>
2024-05-06 12:00:12 +0530

    first push

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,9 @@
 import "@/styles/globals.css";
+import '@/components/landing/style.css'
+import '@/components/footer/style.css'
+import '@/components/navbar/style.css'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-icons/font/bootstrap-icons.css'
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
